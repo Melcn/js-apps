6139a78d0dcddd517025b35ca492e65e2807dce3
Melcn <melaniearinosy@outlook.com>
2023-12-04 10:49:16 +0100

    feat(pokeapi): test fetch log

--- pokeApi/app.js
+++ pokeApi/app.js
@@ -3,6 +3,9 @@ const changePokemon = () => {
     let randomNumber = Math.ceil(Math.random() * 150 + 1) ;
     //On obtient entre 1 et 151
     let requestString = `https://pokeapi.co/api/v2/pokemon/${randomNumber}`; 
+    let data = fetch(requestString);
+
+    console.log(data);
 
 };
 
