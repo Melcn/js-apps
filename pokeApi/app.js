207a63cabeff091eccb4e54fa675195b5b171eb4
Melcn <melaniearinosy@outlook.com>
2023-12-04 10:45:30 +0100

    feat(): random request in appjs

---
+++ pokeApi/app.js
@@ -0,0 +1,9 @@
+let button = document.getElementById("button");
+const changePokemon = () => {
+    let randomNumber = Math.ceil(Math.random() * 150 + 1) ;
+    //On obtient entre 1 et 151
+    let requestString = `https://pokeapi.co/api/v2/pokemon/${randomNumber}`; 
+
+};
+
+button.addEventListener("click", changePokemon);
