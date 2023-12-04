d777a26e0ea9dd3125047ea17e1bbbb5f97c231b
Melcn <melaniearinosy@outlook.com>
2023-12-04 10:52:50 +0100

    feat(pokeapi): test response

--- pokeApi/app.js
+++ pokeApi/app.js
@@ -1,12 +1,14 @@
 let button = document.getElementById("button");
-const changePokemon = () => {
+const changePokemon = async () => {
     let randomNumber = Math.ceil(Math.random() * 150 + 1) ;
     //On obtient entre 1 et 151
     let requestString = `https://pokeapi.co/api/v2/pokemon/${randomNumber}`; 
-    let data = fetch(requestString);
+    let data = await fetch(requestString);
 
     console.log(data);
 
+    let response = await data.json();
+    console.log(response);
 };
 
 button.addEventListener("click", changePokemon);
